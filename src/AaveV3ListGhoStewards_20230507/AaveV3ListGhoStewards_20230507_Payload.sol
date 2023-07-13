6990be2287dadbeeda3e2519970957915a16896f
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-07-13 16:48:38 +0100

    pr comments

--- src/AaveV3ListGhoStewards_20230507/AaveV3ListGhoStewards_20230507_Payload.sol
+++ src/AaveV3ListGhoStewards_20230507/AaveV3ListGhoStewards_20230507_Payload.sol
@@ -1,20 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
+
 import {AaveV3Ethereum, IACLManager} from 'aave-address-book/AaveV3Ethereum.sol';
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {GhoSteward} from 'gho-core/contracts/misc/GhoSteward.sol';
 import {IGhoToken} from 'gho-core/contracts/gho/interfaces/IGhoToken.sol';
 
-library Create2Helpers {
+/**
+ * @title Gho Stewards AIP
+ * @author @aave - AaveCompanies
+ * @dev This proposal enables GhoSteward for agile parameter changes in GHO. It consists of a set of actions:
+ * - Deployment of GhoSteward and transfers ownership to Aave DAO (short executor)
+ * - Grants role of PoolAdmin to GhoSteward
+ * - Grants role of BucketManager to GhoSteward
+ * Relevant governance links:
+ * - Governance: https://governance.aave.com/t/arfc-gho-steward-agile-parameter-changes/13922
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x67fa551993a94b801018d02154f3d4f27e29bea51fe6e862686de6bf9ee650af
+ */
+contract GhoAaveV3GhoSteward is IProposalGenericExecutor {
+  event GhoStewardDeployed(address ghoSteward);
+
+  error DeployedGhoStewardNotEqualPrecomuted(string message);
+
+  address public immutable GHO_STEWARD;
   bytes32 public constant CREATE2_SALT = bytes32(0);
+
+  address public constant RISK_COUNCIL = 0x47c71dFEB55Ebaa431Ae3fbF99Ea50e0D3d30fA8;
+  address public constant GHO_TOKEN = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
+
+  constructor() {
+    GHO_STEWARD = getAddress(
+      getBytecode(
+        address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
+        GHO_TOKEN,
+        RISK_COUNCIL,
+        AaveGovernanceV2.SHORT_EXECUTOR
+      )
+    );
+  }
+
+  function execute() external override {
+    // ------------------------------------------------
+    // 1. Deployment of GhoSteward
+    // ------------------------------------------------
+    address deployedGhoSteward = deployGhoSteward(
+      address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
+      GHO_TOKEN,
+      RISK_COUNCIL,
+      AaveGovernanceV2.SHORT_EXECUTOR
+    );
+
+    if (deployedGhoSteward != GHO_STEWARD) {
+      revert DeployedGhoStewardNotEqualPrecomuted('Contract addresses do not match');
+    }
+
+    // ------------------------------------------------
+    // 2. Set roles of PoolAdmin and BucketManager to GhoSteward
+    // ------------------------------------------------
+    AaveV3Ethereum.ACL_MANAGER.addPoolAdmin(deployedGhoSteward);
+    IGhoToken iGhoToken = IGhoToken(GHO_TOKEN);
+    iGhoToken.grantRole(iGhoToken.BUCKET_MANAGER_ROLE(), deployedGhoSteward);
+  }
 
   function deployGhoSteward(
     address poolAddressProvider,
     address ghoToken,
     address riskCouncil,
     address shortExecutor
-  ) external returns (address) {
+  ) internal returns (address) {
     GhoSteward _ghoStewardContract = new GhoSteward{salt: CREATE2_SALT}(
       poolAddressProvider,
       ghoToken,
@@ -24,7 +78,21 @@ library Create2Helpers {
     return address(_ghoStewardContract);
   }
 
-  function getAddress(bytes memory bytecode) public pure returns (address) {
+  function getBytecode(
+    address poolAddressProvider,
+    address ghoToken,
+    address riskCouncil,
+    address shortExecutor
+  ) internal pure returns (bytes memory) {
+    bytes memory bytecode = type(GhoSteward).creationCode;
+    return
+      abi.encodePacked(
+        bytecode,
+        abi.encode(poolAddressProvider, ghoToken, riskCouncil, shortExecutor)
+      );
+  }
+
+  function getAddress(bytes memory bytecode) internal pure returns (address) {
     bytes32 hash = keccak256(
       abi.encodePacked(
         bytes1(0xff),
@@ -34,72 +102,5 @@ library Create2Helpers {
       )
     );
     return address(uint160(uint256(hash)));
-  }
-
-  function getBytecode(
-    address poolAddressProvider,
-    address ghoToken,
-    address riskCouncil,
-    address shortExecutor
-  ) public pure returns (bytes memory) {
-    bytes memory bytecode = type(GhoSteward).creationCode;
-    return
-      abi.encodePacked(
-        bytecode,
-        abi.encode(poolAddressProvider, ghoToken, riskCouncil, shortExecutor)
-      );
-  }
-}
-
-/**
- * @title Gho Stewards AIP
- * @author @aave - Aave Co
- * @dev This proposal deploys GhoSteward, which consists of a set of actions.
- * - Deployment of GhoSteward and transfers ownership to Aave DAO (short executor)
- * - Grants role of PoolAdmin to GhoSteward
- * - Grants role of BucketManager to GhoSteward
- * Relevant governance links:
- * - Governance: https://governance.aave.com/t/arfc-gho-steward-agile-parameter-changes/13922
- * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x67fa551993a94b801018d02154f3d4f27e29bea51fe6e862686de6bf9ee650af
- */
-
-contract AaveV3ListGhoStewardsPayload is IProposalGenericExecutor {
-  event GhoStewardDeployed(address ghoSteward);
-
-  bytes public GHO_STEWARD_BYTECODE;
-  address public immutable GHO_STEWARD;
-
-  constructor() {
-    GHO_STEWARD_BYTECODE = Create2Helpers.getBytecode(
-      address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
-      GHO_TOKEN,
-      RISK_COUNCIL,
-      AaveGovernanceV2.SHORT_EXECUTOR
-    );
-
-    GHO_STEWARD = Create2Helpers.getAddress(GHO_STEWARD_BYTECODE);
-  }
-
-  address public constant RISK_COUNCIL = 0x47c71dFEB55Ebaa431Ae3fbF99Ea50e0D3d30fA8;
-  address public constant GHO_TOKEN = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
-
-  function execute() external override {
-    // ------------------------------------------------
-    // 1. Deployment of GhoSteward
-    // ------------------------------------------------
-    address deployedGhoSteward = Create2Helpers.deployGhoSteward(
-      address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
-      GHO_TOKEN,
-      RISK_COUNCIL,
-      AaveGovernanceV2.SHORT_EXECUTOR
-    );
-
-    // ------------------------------------------------
-    // 2. Set roles of PoolAdmin and BucketManager to GhoSteward
-    // ------------------------------------------------
-    AaveV3Ethereum.ACL_MANAGER.addPoolAdmin(address(deployedGhoSteward));
-
-    IGhoToken iGhoToken = IGhoToken(GHO_TOKEN);
-    iGhoToken.grantRole(iGhoToken.BUCKET_MANAGER_ROLE(), address(deployedGhoSteward));
   }
 }
--- src/AaveV3ListGhoStewards_20230507/AaveV3ListGhoStewards_20230507_Payload.t.sol
+++ src/AaveV3ListGhoStewards_20230507/AaveV3ListGhoStewards_20230507_Payload.t.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.16;
-
+import 'forge-std/console.sol';
 import 'forge-std/Test.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {ProtocolV3TestBase, InterestStrategyValues, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
-import {AaveV3ListGhoStewardsPayload, Create2Helpers} from './AaveV3ListGhoStewards_20230507_Payload.sol';
+import {GhoAaveV3GhoSteward} from './AaveV3ListGhoStewards_20230507_Payload.sol';
 import {IGhoSteward} from 'gho-core/contracts/misc/interfaces/IGhoSteward.sol';
 import {IPoolDataProvider} from 'aave-v3-core/contracts/interfaces/IPoolDataProvider.sol';
 
@@ -15,31 +15,18 @@ import {IGhoDiscountRateStrategy} from 'gho-core/contracts/facilitators/aave/int
 import {GhoInterestRateStrategy} from 'gho-core/contracts/facilitators/aave/interestStrategy/GhoInterestRateStrategy.sol';
 import {IGhoToken} from 'gho-core/contracts/gho/interfaces/IGhoToken.sol';
 
-contract AaveV3ListGhoStewards_20230507_Payload_Test is ProtocolV3TestBase {
-  AaveV3ListGhoStewardsPayload public proposalPayload;
-  address public constant GHO_TOKEN = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
-  address public constant RISK_COUNCIL = 0x47c71dFEB55Ebaa431Ae3fbF99Ea50e0D3d30fA8;
-  address public constant GHO_IR_STRATEGY = 0x16E77D8a7192b65fEd49B3374417885Ff4421A74;
-  bytes public GHO_STEWARD_BYTECODE;
-  address public GHO_STEWARD;
+contract AaveV3ListGhoStewards_20230507_Payload_Test is ProtocolV3TestBase, GhoAaveV3GhoSteward {
+  GhoAaveV3GhoSteward public proposalPayload;
+  address immutable ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
 
   function setUp() public {
     vm.createSelectFork('https://rpc.tenderly.co/fork/4aa4b542-16b5-4fb7-8f75-fc1a0e2e3848');
-    proposalPayload = new AaveV3ListGhoStewardsPayload();
-
-    GHO_STEWARD_BYTECODE = Create2Helpers.getBytecode(
-      address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
-      GHO_TOKEN,
-      RISK_COUNCIL,
-      AaveGovernanceV2.SHORT_EXECUTOR
-    );
-
-    GHO_STEWARD = Create2Helpers.getAddress(GHO_STEWARD_BYTECODE);
+    proposalPayload = new GhoAaveV3GhoSteward();
   }
 
   function testGhoStewardRoles() public {
     bool beforeGhoStewardAdmin = AaveV3Ethereum.ACL_MANAGER.isPoolAdmin(address(GHO_STEWARD));
-    assertEq(false, beforeGhoStewardAdmin);
+    assertFalse(beforeGhoStewardAdmin);
 
     IGhoToken iGhoToken = IGhoToken(GHO_TOKEN);
 
@@ -48,19 +35,19 @@ contract AaveV3ListGhoStewards_20230507_Payload_Test is ProtocolV3TestBase {
       address(GHO_STEWARD)
     );
 
-    assertEq(false, beforeHasBucketManagerRole);
+    assertFalse(beforeHasBucketManagerRole);
 
     GovHelpers.executePayload(vm, address(proposalPayload), AaveGovernanceV2.SHORT_EXECUTOR);
 
     bool afterGhoStewardAdmin = AaveV3Ethereum.ACL_MANAGER.isPoolAdmin(address(GHO_STEWARD));
-    assertEq(true, afterGhoStewardAdmin);
+    assertTrue(afterGhoStewardAdmin);
 
     bool afterHasBucketManagerRole = iGhoToken.hasRole(
       iGhoToken.BUCKET_MANAGER_ROLE(),
       address(GHO_STEWARD)
     );
 
-    assertEq(true, afterHasBucketManagerRole);
+    assertTrue(afterHasBucketManagerRole);
   }
 
   function testUpdateBorrowRates() public {
@@ -94,6 +81,30 @@ contract AaveV3ListGhoStewards_20230507_Payload_Test is ProtocolV3TestBase {
     vm.stopPrank();
   }
 
+  function testUpdateBorrowRatesWhenNotRiskCouncil() public {
+    vm.startPrank(ZERO_ADDRESS);
+    GovHelpers.executePayload(vm, address(proposalPayload), AaveGovernanceV2.SHORT_EXECUTOR);
+
+    IPoolDataProvider iPoolDataProvider = IPoolDataProvider(
+      AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER
+    );
+
+    IGhoSteward ghoSteward = IGhoSteward(GHO_STEWARD);
+
+    address oldInterestStrategy = iPoolDataProvider.getInterestRateStrategyAddress(
+      address(GHO_TOKEN)
+    );
+
+    uint256 oldBorrowRate = GhoInterestRateStrategy(oldInterestStrategy)
+      .getBaseVariableBorrowRate();
+
+    uint256 newBorrowRate = oldBorrowRate + 15;
+    vm.expectRevert('INVALID_CALLER');
+
+    ghoSteward.updateBorrowRate(newBorrowRate);
+    vm.stopPrank();
+  }
+
   function testUpdateBucketCapacity() public {
     vm.startPrank(RISK_COUNCIL);
 
@@ -118,6 +129,28 @@ contract AaveV3ListGhoStewards_20230507_Payload_Test is ProtocolV3TestBase {
     (uint256 capacity, ) = iGhoToken.getFacilitatorBucket(address(GHO_ATOKEN));
 
     assertEq(capacity, newBucketCapacity);
+    vm.stopPrank();
+  }
+
+  function testUpdateBucketCapacityWhenNotRiskCouncil() public {
+    vm.startPrank(ZERO_ADDRESS);
+
+    IPoolDataProvider iPoolDataProvider = IPoolDataProvider(
+      AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER
+    );
+    (address GHO_ATOKEN, , ) = iPoolDataProvider.getReserveTokensAddresses(address(GHO_TOKEN));
+
+    GovHelpers.executePayload(vm, address(proposalPayload), AaveGovernanceV2.SHORT_EXECUTOR);
+
+    IGhoToken iGhoToken = IGhoToken(address(GHO_TOKEN));
+    IGhoSteward ghoSteward = IGhoSteward(GHO_STEWARD);
+
+    (uint256 oldBucketCapacity, ) = iGhoToken.getFacilitatorBucket(address(GHO_ATOKEN));
+
+    uint128 newBucketCapacity = uint128(oldBucketCapacity) + 1;
+    vm.warp(ghoSteward.MINIMUM_DELAY() + 1);
+    vm.expectRevert('INVALID_CALLER');
+    ghoSteward.updateBucketCapacity(newBucketCapacity);
     vm.stopPrank();
   }
 }
